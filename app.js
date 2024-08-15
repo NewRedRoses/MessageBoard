419e4f3065e1d02d8a71e2f513f8521a829beb5e
Mark A. <mark001616@gmail.com>
2024-08-15 15:04:41 -0400

    Add view, data and move PORT declaration

--- app.js
+++ app.js
@@ -2,15 +2,33 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
-// Views
+const PORT = 3000;
+
+// Views setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Data
+const messages = [
+  {
+    text: "Hello!",
+    user: "Armand",
+    added: new Date(),
+  },
+  {
+    text: "How are you doing, Armand?",
+    user: "Gwyn",
+    added: new Date(),
+  },
+];
+
 app.get("/", (req, res) => {
-  res.render("sample");
+  res.render("index", { title: "Mini Message-board", messages: messages });
+});
+app.get("/new", (req, res) => {
+  res.render("form");
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Launched on port: ${PORT}`);
 });
