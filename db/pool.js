db7ac3d9ad158d72dc951c7a5ffd09b4a97e1696
Mark A. <mark001616@gmail.com>
2024-09-01 00:15:23 -0400

    Minimize pool arguments to URL

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,6 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.PROD_DB_URL,
 });
